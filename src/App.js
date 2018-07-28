636b2c20c5032bc757ea88edf27edadbd07b162a
Daniela Küster <daniela.kuester@gmx.net>
2018-07-28 20:23:12 +0200

    Add: Define books variable for Bookshelf

--- src/App.js
+++ src/App.js
@@ -56,7 +56,9 @@ class BooksApp extends Component {
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
-              <Bookshelf />
+              <Bookshelf
+								books={this.state.books}
+							/>
             </div>
             <div className="open-search">
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
